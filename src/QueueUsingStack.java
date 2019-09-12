7800e6552fe232511b1da081c2403c1684b52061
redoy.ahmed <redoyahmed@gmail.com>
2019-09-12 11:06:16 +0600

    queue using stack added

--- src/QueueUsingStack.java
+++ src/QueueUsingStack.java
@@ -6,32 +6,23 @@ public class QueueUsingStack {
     static Stack<Integer> s2 = new Stack<>();
 
     static void enQueue(int x) {
-        // Move all elements from s1 to s2
-        while (!s1.isEmpty()) {
-            s2.push(s1.pop());
-        }
-
         // Push item into s1
         s1.push(x);
-
-        // Push everything back to s1
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
     }
 
     // Dequeue an item from the queue
     static int deQueue() {
-        // if first stack is empty
-        if (s1.isEmpty()) {
-            System.out.println("Q is Empty");
-            System.exit(0);
+        if (s2.isEmpty()) {
+            if (!s1.isEmpty())
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            else {
+                System.out.println("Q is Empty");
+            }
         }
 
-        // Return top of s1
-        int x = s1.peek();
-        s1.pop();
-        return x;
+        return s2.pop();
     }
 
     public static void main(String[] args) {
